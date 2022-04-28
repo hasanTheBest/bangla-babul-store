725cdb8275f34b528502554d4384244719772010
hasanTheBest <rainbowmahmud@gmail.com>
2022-04-28 19:33:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+/**
+ * DATA BASE connection
+ */
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.gnvo2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+/* async function run() {
+  try {
+    await client.connect();
+
+    const productCollection = client
+      .db("productsDB")
+      .collection("productCollection");
+
+    const query = {};
+    const cursor = productCollection.find(query);
+    const products = await cursor.toArray();
+
+    // since this method returns the matched document, not a cursor, print it directly
+    console.log(products.length);
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir); */
+
+app.get("/", async (req, res) => {
+  let products = [];
+  try {
+    await client.connect();
+
+    const productCollection = client
+      .db("productsDB")
+      .collection("productCollection");
+
+    const query = {};
+    const cursor = productCollection.find(query);
+    products = await cursor.toArray();
+    console.log(products.length);
+  } catch (err) {
+    console.err(err.message);
+  }
+
+  res.send("products.length");
+});
+
+app.listen(port, () => {
+  console.log("app is listening ", port);
+});
